807a0e30bf08ad728f03e94684ab61054f3d9681
Eric K <erickirberger@gmail.com>
2022-12-07 13:33:06 -0500

    saving

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,7 +4,9 @@ var weatherApiKey = "3044316f6126db93462603440b6cd43c";
 
 function getParkApi() {
   
-  var parkApiURL = `https://developer.nps.gov/api/v1/parks?parkCode=acad&api_key=${parkApiKey}`;
+  var parkCode = 
+
+  var parkApiURL = `https://developer.nps.gov/api/v1/parks?parkCode=${parkCode}&api_key=${parkApiKey}`;
 
   fetch(parkApiURL)
     .then(function (response) {
@@ -13,28 +15,42 @@ function getParkApi() {
       .then(function (data) {
         console.log(data)
 
+        var activities = response.
+
     });
 
 }
 
 getParkApi();
 
-function getWeatherApi() {
+// function getWeatherApi() {
   
-  var weatherApiURL = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${apiKey}`;
+//   var weatherApiURL = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${weatherApiKey}`;
 
-  fetch(weatherApiURL)
-    .then(function (response) {
-      return response.json();
-    })
-      .then(function (data) {
-        console.log(data)
+//   fetch(weatherApiURL)
+//     .then(function (response) {
+//       return response.json();
+//     })
+//       .then(function (data) {
+//         console.log(data)
+
+// var temperature = response.list[0].main.temp
+
+// console.log(temperature);
+
+// var windSpeed = response.list[0].wind.speed;
+
+// console.log(windSpeed);
+
+// var humidity = response.list[0].main.humidity;
+
+// console.log(humidity);
       
-    });
+//     });
 
-}
+// }
 
-function getWeatherApi();
+// function getWeatherApi();
 
 
 // document.addEventListener('DOMContentLoaded', function() {
