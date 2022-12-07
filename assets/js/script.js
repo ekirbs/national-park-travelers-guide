494b1f03c59792db1af46723ba298169d6d5b329
Eric K <erickirberger@gmail.com>
2022-12-07 10:32:26 -0500

    commiting to move

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,34 @@
 console.log('for github');
+
+var apiKey = 'A9S3i3Pm3275PfUFD8eiisByBw83gq5GlQ20dgGu';
+
+var apiURL = `https://api.nasa.gov/insight_weather/?api_key=${apiKey}&feedtype=json&ver=1.0`;
+
+function getWeather() {
+  fetch(apiURL)
+  .then(response => response.json())
+  .then(data => {
+    console.log(data);
+  })
+};
+
+// function getWeather() {
+//   fetch(apiURL)
+//   .then(response => response.json())  // this '=>' is a promise.
+//   .then(data => {
+//     var {
+//       sol_keys,
+//       validity_checks,
+//       ...solData
+//     } = data
+//     console.log(data);
+//     console.log(solData);
+//     Object.entries(solData).map([sol, data]) => {  
+//       return {
+//         sol: sol,
+//       }
+//     }
+//   })
+// };
+
+getWeather();
