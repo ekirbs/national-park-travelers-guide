eb7b342d08f032c6caa5ec961099d39ff557354b
Eric K <erickirberger@gmail.com>
2022-12-13 09:50:10 -0500

    cleaned script, added nested dropdowns to cover all of the northeast

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,10 @@ var parkApiKey = 'amkrsfXuAU5bolqQY0bTMOV4h2mBIOTqrRvrJVsd';
 var weatherApiKey = "3044316f6126db93462603440b6cd43c";
 var units = 'imperial';
 var lang = 'en';
+var lat;
+var lon;
+var park;
+var btnSource = 1;
 
 // TIME & SEARCH HISTORY DISPLAY FUNCTION ON PAGE LOAD
 function init() {
@@ -14,7 +18,41 @@ function init() {
 };
 
 init();
-$(".dropdown-trigger").dropdown();
+// $('.dropdown-trigger').dropdown();
+ 
+$(function() {
+  $('.dropdown-trigger').dropdown();
+  $('.sub').dropdown({
+      hover: true
+    });
+});
+
+// var images = data.data[0].images;
+
+// var imagesLength = images.length;
+
+// for (var i = 0; i < images.length; i++) {
+
+//   var slideContainer = $('<div class="mySlides fade">')
+//   $('.slideshow-container').append(slideContainer);
+
+//   var numberDiv = $('<div class="numbertext">').text(`${i + 1}/${imagesLength}`);
+//   slideContainer.append(numberDiv);
+
+//   var imgSrc = data.data[0].images[i].url;
+//   var img = $(`<img src="${imgSrc}" id="icon">`)
+//   // var img = $('<img>').src(`${imgSrc}`);
+//   // var img = $('<img src="data.data[0].images[i]">')  // how to source the images from the data?
+//   slideContainer.append(img);
+
+//   var capName = data.data[0].images[i].name;
+//   var textDiv = $('<div class="text">').text(`${capName}`);
+//   slideContainer.append(textDiv);
+
+
+//   var dotSpan = $('<span class="dot" onclick="currentSlide[i + 1]">')
+//   $('#dotContainer').append(dotSpan);
+// }
 
 // $(document).ready(function) {
 // WEATHER DISPLAY FUNCTION
@@ -36,6 +74,10 @@ function displayChosenPark() {
     })
     .then(function (data) {
       console.log(data);
+
+      lat = data.data[0].latitude.valueOf();
+      lon = data.data[0].longitude.valueOf();
+      console.log(lat, lon);
 
       var activities = data.data[0].activities;
       console.log(activities)
@@ -86,35 +128,9 @@ function displayChosenPark() {
         + "Friday: " + hours.friday + "<br/>"
         + "Saturday: " + hours.saturday + "<br/>")
 
+        displayChosenCity();
     })
 }
-
-      // var images = data.data[0].images;
-
-      // var imagesLength = images.length;
-
-      // for (var i = 0; i < images.length; i++) {
-
-      //   var slideContainer = $('<div class="mySlides fade">')
-      //   $('.slideshow-container').append(slideContainer);
-
-      //   var numberDiv = $('<div class="numbertext">').text(`${i + 1}/${imagesLength}`);
-      //   slideContainer.append(numberDiv);
-
-      //   var imgSrc = data.data[0].images[i].url;
-      //   var img = $(`<img src="${imgSrc}" id="icon">`)
-      //   // var img = $('<img>').src(`${imgSrc}`);
-      //   // var img = $('<img src="data.data[0].images[i]">')  // how to source the images from the data?
-      //   slideContainer.append(img);
-
-      //   var capName = data.data[0].images[i].name;
-      //   var textDiv = $('<div class="text">').text(`${capName}`);
-      //   slideContainer.append(textDiv);
-
-
-      //   var dotSpan = $('<span class="dot" onclick="currentSlide[i + 1]">')
-      //   $('#dotContainer').append(dotSpan);
-      // }
 
 // WEATHER DISPLAY FROM HISTORY FUNCTION
 function displayChosenCity() {
@@ -122,11 +138,12 @@ function displayChosenCity() {
   $("#feature-spot").empty();
 
   $('#weather').empty();
+  console.log(lat, lon)
 
   // if input is dropdown btn, use that city, else if input is history button, use data-name
-  // var city = $(this).attr('data-name');
   // var cityName = $("#search-input").val().trim();
-  var weatherApiURL = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${weatherApiKey}&units=${units}&lang=${lang}`;
+  var weatherApiURL = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${weatherApiKey}&units=${units}&lang=${lang}`;
+  // var weatherApiURL = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${weatherApiKey}&units=${units}&lang=${lang}`;
 
   fetch(weatherApiURL)
     .then(function (response) {
@@ -137,7 +154,7 @@ function displayChosenCity() {
       var featureCard = $("<div class='card feature-card zoom'>");
 
       var name = data.city.name;
-      var city = $('<h1>').text(name);
+      var city = $('<h2>').text(name);
       featureCard.append(city);
         
       var dateDisplay = $('<h2>').text((dayjs()).format('M/D/YYYY'));    
@@ -198,8 +215,11 @@ function displayChosenCity() {
 };
 
 // GET LOCALSTORAGE OF SEARCH HISTORY FUNCTION
-function getLocalStorage() {
+function getLocalNameStorage() {
   return JSON.parse(localStorage.getItem("cities")) || [];
+};
+function getLocalCodeStorage() {
+  return JSON.parse(localStorage.getItem("code")) || [];
 };
 
 // RENDER SEARCH HISTORY FUNCTION
@@ -207,15 +227,19 @@ function renderHistory() {
       
   $('#history-spot').empty();
 
-  var cities = getLocalStorage();
+  var cities = getLocalNameStorage();
+  var code = getLocalCodeStorage();
     
   for (i = 0; i < cities.length; i++) {
       
-    var histButton = $('<button class="hist-btn btn">');
+    var histButton = $(`<button class="hist-btn btn">`);
       
     histButton.addClass('hist-btn');
+
+    // $(`#hist${i}`).attr('class', park);
       
     histButton.attr('data-name', cities[i]);
+    histButton.attr('data-code', code[i]);
       
     histButton.text(cities[i]);
       
@@ -225,14 +249,18 @@ function renderHistory() {
 };
 
 // ON BUTTON CLICK, ADD SEARCHED CITIES TO HISTORY AND RENDER HISTORY & WEATHER
+// $('.dropdown-btn').on('click', function (event) {
 $('.dropdown-btn').on('click', function (event) {
           
   event.preventDefault();
         
-  var cities = getLocalStorage();
+  var code = getLocalCodeStorage();
+  var cities = getLocalNameStorage();
+
+  park = event.target.id;
+  console.log(park)
   
-  console.log(cities);
- 
+  console.log(cities); 
 
   var city = event.target.innerHTML;
   console.log(city);
@@ -241,20 +269,35 @@ $('.dropdown-btn').on('click', function (event) {
     console.log("already chosen");
   } else {
     cities.push(city);
-    console.log(cities);
+    code.push(park);
+    console.log(cities, park);
     
     if (cities.length > 5) {  
       cities.shift();
+      code.shift();
     };
 
     localStorage.setItem("cities", JSON.stringify(cities));
+    localStorage.setItem("code", JSON.stringify(code));
+
   };
-  
+
+  displayChosenPark();
   renderHistory();
 })
 
 // WEATHER DISPLAY WHEN SEARCH HISTORY IS CLICKED
-$(document).on('click', '.hist-btn', displayChosenCity);
+$(document).on('click', '.hist-btn', function(event) {
+// $(document).on('click', '.hist-btn', displayChosenPark);
+// $('.hist-btn').on('click', function (event) {
+          
+  event.preventDefault();
+
+  park = $(this).attr('data-code');
+  console.log(park);
+
+  displayChosenPark();
+})
 
 let slideIndex = 1;
 showSlides(slideIndex);
@@ -283,123 +326,124 @@ function showSlides(n) {
 };
 
 // DROPDOWN BUTTON CLICKS
-$("#acad").on("click", function (event) {
-  event.preventDefault();
 
-  park = 'ACAD';
-  city = 'Bar Harbor, Maine';
+// $("#acad").on("click", function (event) {
+//   event.preventDefault();
 
-  displayChosenPark();
-  displayChosenCity();
+//   park = 'ACAD';
+//   city = 'Bar Harbor, Maine';
 
-});
+//   displayChosenPark();
+//   displayChosenCity();
 
-$("#arch").on("click", function (event) {
-  event.preventDefault();
+// });
 
-  park = "ARCH";
-  city = "Moab,Utah";
+// $("#arch").on("click", function (event) {
+//   event.preventDefault();
 
-  displayChosenPark();
-  displayChosenCity();
+//   park = "ARCH";
+//   city = "Moab, Utah";
 
-});
+//   displayChosenPark();
+//   displayChosenCity();
 
-$("#dena").on("click", function (event) {
-  event.preventDefault();
+// });
 
-  park = "DENA";
-  city = "Healy, Alaska";
+// $("#dena").on("click", function (event) {
+//   event.preventDefault();
 
-  displayChosenPark();
-  displayChosenCity();
+//   park = "DENA";
+//   city = "Healy, Alaska";
 
-});
+//   displayChosenPark();
+//   displayChosenCity();
 
-$("#ever").on("click", function (event) {
-  event.preventDefault();
+// });
 
-  park = "EVER";
-  city = "Everglades City, Florida";
+// $("#ever").on("click", function (event) {
+//   event.preventDefault();
 
-  displayChosenPark();
-  displayChosenCity();
+//   park = "EVER";
+//   city = "Florida City, Florida";
 
-});
+//   displayChosenPark();
+//   displayChosenCity();
 
-$("#glac").on("click", function (event) {
-  event.preventDefault();
+// });
 
-  park = "GLAC";
-  city = "Columbia Falls, Montana";
+// $("#glac").on("click", function (event) {
+//   event.preventDefault();
 
-  displayChosenPark();
-  displayChosenCity();
+//   park = "GLAC";
+//   city = "Columbia Falls, Montana";
 
-});
+//   displayChosenPark();
+//   displayChosenCity();
 
-$("#glca").on("click", function (event) {
-  event.preventDefault();
+// });
 
-  park = "GLCA";
-  city = "Tusayan, Arizona";
+// $("#glca").on("click", function (event) {
+//   event.preventDefault();
 
-  displayChosenPark();
-  displayChosenCity();
+//   park = "GLCA";
+//   city = "Tusayan, Arizona";
 
-});
+//   displayChosenPark();
+//   displayChosenCity();
 
-$("#redw").on("click", function (event) {
-  event.preventDefault();
+// });
 
-  park = "REDW";
-  city = "Crescent City, California";
+// $("#redw").on("click", function (event) {
+//   event.preventDefault();
 
-  displayChosenPark();
-  displayChosenCity();
+//   park = "REDW";
+//   city = "Crescent City, California";
 
-});
+//   displayChosenPark();
+//   displayChosenCity();
 
-$("#romo").on("click", function (event) {
-  event.preventDefault();
+// });
 
-  park = "ROMO";
-  city = "Denver, Colorado";
+// $("#romo").on("click", function (event) {
+//   event.preventDefault();
 
-  displayChosenPark();
-  displayChosenCity();
+//   park = "ROMO";
+//   city = "Denver, Colorado";
 
-});
+//   displayChosenPark();
+//   displayChosenCity();
 
-$("#seki").on("click", function (event) {
-  event.preventDefault();
+// });
 
-  park = "SEKI";
-  city = "Visalia,California";
+// $("#seki").on("click", function (event) {
+//   event.preventDefault();
 
-  displayChosenPark();
-  displayChosenCity();
+//   park = "SEKI";
+//   city = "Visalia, California";
 
-});
+//   displayChosenPark();
+//   displayChosenCity();
 
-$("#yell").on("click", function (event) {
-  event.preventDefault();
+// });
 
-  park = "YELL";
-  city = "Gardiner, Montana";
+// $("#yell").on("click", function (event) {
+//   event.preventDefault();
 
-  displayChosenPark();
-  displayChosenCity();
+//   park = "YELL";
+//   city = "Gardiner, Montana";
 
-});
+//   displayChosenPark();
+//   displayChosenCity();
 
-$("#yose").on("click", function (event) {
-  event.preventDefault();
+// });
 
-  park = "YOSE";
-  city = "Mariposa, California";
+// $("#yose").on("click", function (event) {
+//   event.preventDefault();
 
-  displayChosenPark();
-  displayChosenCity();
+//   park = "YOSE";
+//   city = "Mariposa, California";
 
-});
+//   displayChosenPark();
+//   displayChosenCity();
+
+// });
